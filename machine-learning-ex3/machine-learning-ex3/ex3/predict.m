c15cc1f25359495a6a28576ca7a5fb531d07946e
anant1407 <70603708+anant1407@users.noreply.github.com>
2020-11-10 20:10:27 +0530

    Add files via upload

---
+++ machine-learning-ex3/machine-learning-ex3/ex3/predict.m
@@ -0,0 +1,68 @@
+function p = predict(Theta1, Theta2, X)
+%PREDICT Predict the label of an input given a trained neural network
+%   p = PREDICT(Theta1, Theta2, X) outputs the predicted label of X given the
+%   trained weights of a neural network (Theta1, Theta2)
+
+% Useful values
+m = size(X, 1);
+num_labels = size(Theta2, 1);
+
+% You need to return the following variables correctly 
+p = zeros(size(X, 1), 1);
+size(Theta1);
+size(Theta2);
+size(X);
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Complete the following code to make predictions using
+%               your learned neural network. You should set p to a 
+%               vector containing labels between 1 to num_labels.
+%
+% Hint: The max function might come in useful. In particular, the max
+%       function can also return the index of the max element, for more
+%       information see 'help max'. If your examples are in rows, then, you
+%       can use max(A, [], 2) to obtain the max for each ro
+
+X = [ones(size(X,1),1),X];
+z1 = Theta1*X';
+a2 = sigmoid(z1);
+size(a2);
+a2 = [ones(1,size(a2,2));a2];
+size(a2);
+z2 = Theta2*a2;
+a3 = sigmoid(z2);
+h = a3;
+
+
+j=1;
+
+while j <= m
+  max = 0;c = 1;
+  while c <= num_labels
+    
+    if h(c,j) > max
+      max = h(c,j);
+      p(j,1) = c;
+    endif
+    
+    c = c+1;
+  
+  endwhile
+
+  j = j+1;
+
+endwhile
+
+
+
+
+
+
+
+
+
+
+% =========================================================================
+
+
+end
